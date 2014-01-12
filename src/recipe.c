63222d34c92df84c37fce58bd5e14510fa4f7fdc
blairaggie <blairaggie@gmail.com>
2014-01-12 13:04:57 -0600

    Created test files

--- src/recipe.c
+++ src/recipe.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 /*Simple test as of right now.  */
-int main(){
-    printf("Test...");
-    getchar();
-    return 0;
-    exit(0);
+int main( int argc, char *argv[] ){
+    int sys;
+    for (sys=0; sys<2; sys++ ){
+        int sysio;
+        FILE *file = fopen( argv[1], "r" );
+        while ( ( sysio = fgetc( file ) ) != EOF){
+            printf("%c", sysio);
+        }
+        fclose( file );
+    }
 }
